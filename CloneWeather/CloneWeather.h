f551a7957b810e812958069b69bf10bbf5abaa2a
qazi <qazi.naveed@nextgeni.com>
2024-12-24 10:30:38 +0100

    Initial Commit

---
+++ CloneWeather/CloneWeather.h
@@ -0,0 +1,18 @@
+//
+//  CloneWeather.h
+//  CloneWeather
+//
+//  Created by Qazi on 24/12/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CloneWeather.
+FOUNDATION_EXPORT double CloneWeatherVersionNumber;
+
+//! Project version string for CloneWeather.
+FOUNDATION_EXPORT const unsigned char CloneWeatherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloneWeather/PublicHeader.h>
+
+
---
+++ CloneWeatherTests/CloneWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloneWeatherTests.swift
+//  CloneWeatherTests
+//
+//  Created by Qazi on 24/12/2024.
+//
+
+import Testing
+@testable import CloneWeather
+
+struct CloneWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
