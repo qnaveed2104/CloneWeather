6b17bc68458533010da35e38ae7b75141006b553
qazi <qazi.naveed@nextgeni.com>
2024-12-26 12:52:50 +0100

    remaining file

---
+++ CloneWeather/Sources/CloneWeather/CloneWeather+Ext.swift
@@ -0,0 +1,31 @@
+//
+//  CloneWeather+Ext.swift
+//  CloneWeather
+//
+//  Created by Qazi on 24/12/2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension CloneWeatherClass {
+//    // MARK: - Internal Helper Methods (Acts like decode logic)
+//    internal func createWeatherViewModel() -> AnyView? {
+//        return nil
+//    }
+//    
+//    public enum WeatherSDKError: Error, LocalizedError {
+//        case invalidAPIKey
+//        case invalidCityName
+//        
+//        public var errorDescription: String? {
+//            switch self {
+//            case .invalidAPIKey:
+//                return "The API key provided is invalid."
+//            case .invalidCityName:
+//                return "The city name provided is invalid."
+//            }
+//        }
+//    }
+}
+
