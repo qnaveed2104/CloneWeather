e4cea7f89bb645589fa31f736c1fb4e94c6ab578
qazi <qazi.naveed@nextgeni.com>
2024-12-26 12:56:28 +0100

    adding on path

---
+++ Package.swift
@@ -0,0 +1,36 @@
+//
+//  Package.swift
+//  CloneWeather
+//
+//  Created by Qazi on 26/12/2024.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "CloneWeather",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "CloneWeather",
+            targets: ["CloneWeather"]
+        ),
+    ],
+    dependencies: [
+        // Add any dependencies here
+    ],
+    targets: [
+        .target(
+            name: "CloneWeather",
+            url: "https://github.com/qnaveed2104/CloneWeather.git",
+            checksum: "ae4139dd0ff33009d82b12c12c888d8350efb0774379d8261268e4f78fdc7c14",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "CloneWeatherTests",
+            dependencies: ["CloneWeather"]
+        ),
+    ]
+)
